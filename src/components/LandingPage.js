0066ceba5c7e7f46c23719473c577e315e7a3626
Goncalo <mysterio.55@hotmail.com>
2021-11-25 22:05:51 +0000

    finished swiper on first div

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "../style/LandingPage.css";
-import BigCard from "./BigCard";
+import MiniCard from "./MiniCard";
 import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
+import "swiper/swiper.scss";
+import SwiperCore, { Navigation } from "swiper";
+import "swiper/modules/navigation/navigation.scss";
+
+SwiperCore.use([Navigation]);
 
 const LandingPage = () => {
   const [miniList, setMiniList] = useState([]);
@@ -17,13 +23,54 @@ const LandingPage = () => {
     fetchData();
   }, []);
   return (
-    <>
-      <h1>hello</h1>
+    <section className="total-container">
+      <div className="container">
+        <div className="box1">
+          <h3> Welcome Vania </h3>
+          <p>Country </p>
+        </div>
+        <div className="box2"> Moon / Star </div>
+      </div>
+      <div className="divbody">
+        <ul className="search-ul">
+          <Swiper
+            className="swiper-test"
+            navigation={false}
+            spaceBetween={1}
+            breakpoints={{
+              320: { slidesPerView: 2 },
+              375: { slidesPerView: 3 },
+              480: { slidesPerView: 4 },
+              768: { slidesPerView: 3 },
+              1024: { slidesPerView: 5 },
+            }}
+          >
+            <div className="swiper-items">
+              {miniList &&
+                miniList.map((event) => (
+                  <SwiperSlide>
+                    <li>
+                      {" "}
+                      <MiniCard key={event.id} event={event} />
+                    </li>
+                  </SwiperSlide>
+                ))}
+            </div>
+          </Swiper>
+        </ul>
+      </div>
+
+      <div className="container2">
+        <div className="country"> search country</div>
+        <div className="categories"> search category</div>
+      </div>
+
+      {/* <h1>hello</h1>
       <div>
         {miniList &&
-          miniList.map((event) => <BigCard key={event.id} event={event} />)}
-      </div>
-    </>
+          miniList.map((event) => <MiniCard key={event.id} event={event} />)}
+      </div> */}
+    </section>
   );
 };
 
--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -3,25 +3,31 @@ import star from "../images/favourite.png";
 
 function MiniCard(props) {
   return (
-    <div>
-      <img
+    <div className="div-cat1">
+      {/* <img
         src={star}
         alt="star"
         width="25px"
         height="25px"
         onClick={(element) => (element.target.style.backgroundColor = "yellow")}
-      ></img>
-      <div className="card-name">{props.event.name}</div>
-      <div className="card-image"></div>
-      <img src={props.event.images[0].url} alt="" />
-      <div className="card-locale">
+      ></img> */}
+
+      {/* <div className="card-name">{props.event.name}</div>
+      <div className="card-image"></div> */}
+      <img
+        src={props.event.images[0].url}
+        alt=""
+        width="100px"
+        height="100px"
+      />
+      {/* <div className="card-locale">
         {props.event._embedded.venues[0].address.line1} <br />
         {props.event._embedded.venues[0].address.line2}{" "}
       </div>
       <div className="card-time">
         {props.event.dates.start.localDate} <br />
         {props.event.dates.start.localTime}
-      </div>
+      </div> */}
     </div>
   );
 }
