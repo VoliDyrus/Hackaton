f1910deee6344d3030768dacb445e64a27862837
David <davidjspinto92@gmail.com>
2021-11-25 21:27:51 +0000

    landing-page css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CountrySearch from "./CountrySearch";
-
 import { countriesCode } from "../data/countriesData";
+import logo from "../logo.png";
+
+import "../style/Main.css";
 
 
 function Main() {
@@ -17,8 +19,11 @@ function Main() {
     setSelectedCountry(e.target.elements.country.value)
     setUserName(e.target.value)
   }
-
- 
+  function handleRedirect (url) {
+    return(
+        window.open(url)
+    )
+  }
   async function requestApi(selectedCountry) {
     let countryFound = countriesCode.find(
       (country) => country.name === selectedCountry
@@ -40,37 +45,49 @@ function Main() {
   }, [selectedCountry]);
 
   return (
-    <div>
-      <div className="slider">
-        <div className="slider-content">
-          <h2 className="slider-title">
-            <strong>Select your Country</strong>
-          </h2>
-          <br />
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="userName"
-              value={userName}
-              onChange={(e) => console.log(e.target.value)}
-            />           
-            <input
-              type="text"
-              list="country-list"
-              name="country"
-              id="country"
-              onChange={(e) => console.log(e.target.value)}
-              data-code="code-test"
-            />
-            <datalist id="country-list" onSelect={(e) => console.log(e.target)}>
-              {countriesCode
-                .filter((country) => country.name.startsWith(filterType))
-                .map((country) => (
-                  <option key={country.code} value={country.name} />
-                ))}
-            </datalist>
-            <button type="submit">Enter</button>
-          </form>
+    <div className="page-display">
+      <img className="logo-props" src={logo} alt=""/>
+      <br />
+      <form className="form-style" onSubmit={handleSubmit}>
+        <input className="input-container"
+          type="text"
+          name="userName"
+          value={userName}
+          onChange={(e) => console.log(e.target.value)}
+          placeholder="Write your name"
+        /><br />        
+        <input className="input-container"
+          type="text"
+          list="country-list"
+          name="country"
+          id="country"
+          onChange={(e) => console.log(e.target.value)}
+          data-code="code-test"
+          placeholder="Select a city"
+        /><br /> 
+        <datalist id="country-list" onSelect={(e) => console.log(e.target)}>
+          {countriesCode
+            .filter((country) => country.name.startsWith(filterType))
+            .map((country) => (
+              <option key={country.code} value={country.name} />
+            ))}
+        </datalist><br /> 
+          <button className="btn-container" type="submit">Enter</button>
+      </form>
+      <div className="social-media">
+        <div className="wrapper">
+          <div className="button">
+              <div className="icon"><i className="fab fa-facebook-f"></i></div>
+              <a className="removeHiperLinks" onClick={()=>{handleRedirect("https://www.facebook.com/")}}><span>Facebook</span></a>
+          </div>
+          <div className="button">
+              <div className="icon"><i className="fab fa-instagram"></i></div>
+              <a className="removeHiperLinks" onClick={()=>{handleRedirect("https://www.instagram.com/")}}><span>Instagram</span></a>
+          </div>
+          <div className="button">
+              <div className="icon"><i className="fab fa-github"></i></div>
+              <a className="removeHiperLinks" onClick={()=>{handleRedirect("https://github.com/")}}><span>GitHub</span></a>
+          </div>
         </div>
       </div>
     </div>
