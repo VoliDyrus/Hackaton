984ee8aaf2ba1f61d7bd26ecc2bebf416bbf8d71
Diogo <hyperdg12@gmail.com>
2021-11-25 17:51:44 +0000

    added filter to main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,70 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CountrySearch from "./CountrySearch";
 
-import CardDetails from './CardDetails';
+import { countriesCode } from "../data/countriesData";
 
 function Main() {
-  const { 
-    register, 
-    handleSubmit, 
-    formState: { errors } } = useForm();
-  
-  const onSubmit = data => console.log(data);
-  console.log(errors);
-  
+  const [filterType, setFilterType] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState("Great Britain");
+
+  console.log("selectedCountry", selectedCountry);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSelectedCountry(e.target.elements.country.value);
+  }
+
+  async function requestApi(selectedCountry) {
+    let countryFound = countriesCode.find(
+      (country) => country.name === selectedCountry
+    );
+
+    if (countryFound) {
+      const response = await axios.get(
+        `https://app.ticketmaster.com/discovery/v2/events.json?classificationId=KZFzniwnSyZfZ7v7nJ&countryCode=${countryFound.code}&size=10&apikey=qrf4AHhPNz3OMCpLMaTadNgQxJNSHmkc`
+      );
+      const data = await response.data;
+      console.log(data);
+    } else {
+      alert("Pls select valid country");
+    }
+  }
+
+  useEffect(() => {
+    requestApi(selectedCountry);
+  }, [selectedCountry]);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input 
-        type="text" 
-        placeholder="Name" {...register("Name", {required: true})} /><br />
-        {errors.Name && (
-            <span className="red">
-              This field is required and with the right format
-            </span>
-          )}<br /><br />
-
-        
-      <button type="submit">Enter</button>
-      <CardDetails />
-      
-    </form>
-
-    
+    <div>
+      <div className="slider">
+        <div className="slider-content">
+          <h2 className="slider-title">
+            <strong>Select your Country</strong>
+          </h2>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              list="country-list"
+              name="country"
+              id="country"
+              onChange={(e) => console.log(e.target.value)}
+              data-code="code-test"
+            />
+            <datalist id="country-list" onSelect={(e) => console.log(e.target)}>
+              {countriesCode
+                .filter((country) => country.name.startsWith(filterType))
+                .map((country) => (
+                  <option key={country.code} value={country.name} />
+                ))}
+            </datalist>
+            <input type="submit" value="Search" className="slider-search-btn" />
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
-
 
 export default Main;
