7ddeb942ae09ed888db861822fbbec9ad04db421
InesMMJ <inesmmj97@gmail.com>
2021-11-25 17:08:18 +0000

    added star

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,7 +1,16 @@
 import React from "react";
+import star from "../images/favourite.png";
+
 function MiniCard(props) {
   return (
     <div>
+      <img
+        src={star}
+        alt="star"
+        width="25px"
+        height="25px"
+        onClick={(element) => (element.target.style.backgroundColor = "yellow")}
+      ></img>
       <div className="card-name">{props.event.name}</div>
       <div className="card-image"></div>
       <img src={props.event.images[0].url} alt="" />
